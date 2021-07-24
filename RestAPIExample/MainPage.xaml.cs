6238b8ef806ca0a0a2699b390fdba3c0622ff1ea
vishnu <vishnudevxamarin.gmail.com>
2021-07-24 20:40:44 +0530

    Add project files.

---
+++ RestAPIExample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RestAPIExample.models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestAPIExample
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<Root> Post { get; set; }
+        public MainPage()
+        {
+
+            InitializeComponent();
+            GetData();
+        }
+        public async Task GetData()
+        {
+            HttpClient client;
+
+            Uri uri = new Uri(string.Format("https://jsonplaceholder.typicode.com/posts", string.Empty));
+            client = new HttpClient();
+
+            //string json = JsonSerializer.Serialize<TodoItem>(item, serializerOptions);
+            // StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+             response = await client.GetAsync(uri);
+            
+
+            if (response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine(@"\tTodoItem successfully saved.");
+                string content = await response.Content.ReadAsStringAsync();
+               var Items = JsonConvert.DeserializeObject<List<Root>>(content);
+                Post = new ObservableCollection<Root>(Items);
+                lstPost.ItemsSource = Post;
+            }
+        }
+    }
+}
---
+++ RestAPIExample/models/Root.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestAPIExample.models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Root
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+
+}
